920e5295d8a352bb55ea3fe79959db9bc1149a03
Matheus Miranda <matheusrm.ws@gmail.com>
2025-10-14 19:02:53 -0300

    "Fixing module problem"

--- cinema/Movie Recommender/streamlit_app/movie_night.py
+++ cinema/Movie Recommender/streamlit_app/movie_night.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import model_keras.recommender_model as rm
+from model_keras import recommender_model as rm
 import keras
 from threading import RLock
 import joblib

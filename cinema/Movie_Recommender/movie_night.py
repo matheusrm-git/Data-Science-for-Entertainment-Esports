1d934143028e7ad20e1b0c635c53d29c33d5ceb7
Matheus Miranda <matheusrm.ws@gmail.com>
2025-10-14 23:20:13 -0300

    Changing logo img

--- cinema/Movie_Recommender/movie_night.py
+++ cinema/Movie_Recommender/movie_night.py
@@ -209,7 +209,7 @@ st.set_page_config(page_title='Movie Night', layout='wide')
 
 # Title
 with st.container(horizontal=True, horizontal_alignment='center'):
-    with st.container(horizontal=True, horizontal_alignment='center',width=800, height=450):
+    with st.container(horizontal=True, horizontal_alignment='center'):
         #st.title('_Movie Night !_')
         st.image('cinema/Movie_Recommender/img/movie_night_logo.png')
 

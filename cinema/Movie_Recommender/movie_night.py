4d7ae50eb14c14dbdf68c65c0040f407bca166f3
Matheus Miranda <matheusrm.ws@gmail.com>
2025-10-14 23:23:29 -0300

    Reshape logo

--- cinema/Movie_Recommender/movie_night.py
+++ cinema/Movie_Recommender/movie_night.py
@@ -208,8 +208,8 @@ user_dict = {x:0.0 for x in USER_COLS}
 st.set_page_config(page_title='Movie Night', layout='wide')
 
 # Title
-with st.container(horizontal=True, horizontal_alignment='center'):
-    with st.container(horizontal=True, horizontal_alignment='center'):
+with st.container(horizontal=True, horizontal_alignment='center',height='content'):
+    with st.container(horizontal=True, horizontal_alignment='center', height='content'):
         #st.title('_Movie Night !_')
         st.image('cinema/Movie_Recommender/img/movie_night_logo.png')
 
